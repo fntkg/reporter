ae3d1d21ad7583ef594e49e1d4eb0e8bcb6ee946
Germán Garcés <757024@unizar.es>
2021-06-28 11:03:34 +0200

    Update handler_test.go

--- cmd/grafana-reporter/handler_test.go
+++ cmd/grafana-reporter/handler_test.go
@@ -25,8 +25,8 @@ import (
 	"net/url"
 	"testing"
 
-	"github.com/IzakMarais/reporter/grafana"
-	"github.com/IzakMarais/reporter/report"
+	"github.com/fntkg/reporter/grafana"
+	"github.com/fntkg/reporter/report"
 	"github.com/gorilla/mux"
 	. "github.com/smartystreets/goconvey/convey"
 )
